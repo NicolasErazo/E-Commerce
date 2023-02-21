e359979285237890ef3fec731ff60875cb88193c
Nicolas Erazo <nico.lacho@outlook.com>
2023-02-21 18:10:34 -0500

    Update

--- src/app/website/pages/login/login.component.ts
+++ src/app/website/pages/login/login.component.ts
@@ -3,7 +3,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import Swal from 'sweetalert2';
-import { MyValidators } from './../../../utils/validators'
 
 @Component({
   selector: 'app-login',
